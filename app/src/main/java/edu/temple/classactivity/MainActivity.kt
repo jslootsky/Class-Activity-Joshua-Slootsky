4288164debd2e4d36d3ee4835569dbf3ffe3194d
jslootsky <joshuaslootsky@gmail.com>
2025-01-28 12:16:39 -0500

    Gave functionality to "click me" button that changed its text to "Button Clicked" when clicked

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -2,10 +2,10 @@ package edu.temple.classactivity
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+//import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
